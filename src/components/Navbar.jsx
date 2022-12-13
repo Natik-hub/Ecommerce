ba4dfa472105be6d0d90715081e67d4cacd2129a
Natnael Kibru <nathnaelkibru@gmail.com>
2022-12-13 21:06:23 +0300

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,91 @@
-import React from 'react'
-import styled from 'styled-components'
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import { Badge } from '@mui/material';
+import React from "react";
+import styled from "styled-components";
 
-const Container = styled.div `
- height : 60px;
-`
-const Wrapper = styled.div `
- padding:10px 20px;
- display: flex;
- justify-content: space-between;
-`
-const Left =styled.div ``
-const Center =styled.div``
-const Right =styled.div ``
+const Container = styled.div`
+  height: 60px;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between; 
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+ 
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-right: 25px;
+
+`;
 
 const Navbar = () => {
   return (
     <Container>
-        <Wrapper>
-            <Left>Left</Left>
-            <Center>Center</Center>
-            <Right>Right</Right>
-        </Wrapper>
-        </Container>
-  )
-}
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input placeholder="Search" />
+            <SearchIcon style={{ color: "gray", fontSize: 16 }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>FUA.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <Badge badgeContent={4} color="primary">
+            <ShoppingCartOutlinedIcon/>
+          </Badge>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
 
-export default Navbar
+export default Navbar;
