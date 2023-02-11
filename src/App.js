d04c81a2b461290c889a1587ac3594c58e782104
Natnael Kibru <nathnaelkibru@gmail.com>
2023-02-11 20:13:21 +0300

    combo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-
-
-import Home from "./pages/Home";
-import Cart from './pages/Cart';
-import Register from "./pages/Register";
-import Login from './pages/Login'
+import React from "react";
 import Product from "./pages/Product";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 import ProductList from "./pages/ProductList";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { BrowserRouter, Route, Routes,Navigate } from "react-router-dom";
 
 
-const App = () =>  {
+const App = () =>  { 
+  const user = true;
+ 
   return (
-    <div>
-    <ProductList />
-    </div>
+   
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/products/:id" element={<Product />} />
+      <Route path="/products/:category" element={<ProductList />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/login" element={user ? <Navigate to="/" replace /> :  <Login />}  />
+      <Route path = "/register" element={user ? <Navigate to="/" replace /> :  <Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
